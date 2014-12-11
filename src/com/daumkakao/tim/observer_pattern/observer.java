2e554c7a306ca34f94f4770f94c0769d0ede6f63
tim-han <juntz@daum.net>
2014-12-11 18:02:50 +0900

    add strategy pattern

--- src/com/daumkakao/tim/observer_pattern/observer.java
+++ src/com/daumkakao/tim/observer_pattern/observer.java
@@ -4,5 +4,7 @@ package com.daumkakao.tim.observer_pattern;
  * Created by yjhan on 14. 12. 11..
  */
 public class observer {
-    int a = 0;
+    public static void main(String[] args) {
+        System.out.println("ASDF");
+    }
 }
---
+++ src/com/daumkakao/tim/strategy_pattern/Main.java
@@ -0,0 +1,8 @@
+package com.daumkakao.tim.strategy_pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("ZXCV");
+    }
+}
